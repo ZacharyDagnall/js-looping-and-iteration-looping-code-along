1431c8c3bc274b70ab5eb79b7d13609fab1efbd8
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-02-22 17:59:19 -0500

    commit tree feac9dd2c888d8a9a20ec1a5c81782df2548adae

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+function writeCards(names) {
+    return names.map(function (name) {
+        return `Thank you, ${name}, for the wonderful surprise gift!`
+    })
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i)
+    }
+}
